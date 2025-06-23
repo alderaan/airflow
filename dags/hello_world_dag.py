29d655a92e2848b183a6b5bb827d78aa2b191f62
David Lucker <david.l@gojek.com>
2025-06-23 20:39:25 +0700

    further testing

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 def hello_world():
-    print("Hello World, David is gonna be super successful.")
+    print("Hello World, David is gonna be super successful. Really. ")
 
 
 default_args = {
