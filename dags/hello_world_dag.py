e53657d7a8757a69a9e085bdf3b2b9c2a2c289b5
David Lucker <david.l@gojek.com>
2025-06-23 21:52:59 +0700

    testinggg

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -1,24 +1,20 @@
-# In your dags/hello_world_dag.py file (replace the old content)
+# In your dags/hello_world_dag.py file
 
 from __future__ import annotations
-
 import pendulum
-
 from airflow.decorators import dag, task
 
 
 @task
 def hello_task():
     """This task prints the NEW hello world message."""
-    # 1. This is your implementation change
     print("Hello World, David has MASTERED Airflow versioning!")
 
 
 @task
 def dummy_structural_task():
     """
-    2. This is a structural change. The existence of this new task
-       will create a new DAG Version in the UI.
+    This is the structural change from the previous version.
     """
     pass
 
@@ -28,18 +24,15 @@ def dummy_structural_task():
     start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
     schedule=None,
     catchup=False,
-    tags=["example"],
+    tags=["example", "v3-test"],  # <-- THE ONLY CHANGE IS ADDING THIS NEW TAG
 )
 def hello_world_dag():
     """
     ### Hello World DAG
     A simple DAG to demonstrate execution and versioning.
-    This is the second version of the DAG, with a structural change.
     """
-    # Call both tasks to include them in the DAG
     hello_task()
     dummy_structural_task()
 
 
-# This final line instantiates the DAG
 hello_world_dag()
