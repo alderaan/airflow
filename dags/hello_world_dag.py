30ba8c571afeadba58c1aba8b3b41c43fd3ba99d
David Lucker <david.l@gojek.com>
2025-06-23 15:58:19 +0700

    added hello world dag, removed example dags

---
+++ dags/hello_world_dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+
+def hello_world():
+    print("Hello World")
+
+
+default_args = {
+    "start_date": datetime(2024, 1, 1),
+}
+
+dag = DAG(
+    "hello_world",
+    default_args=default_args,
+    description="A simple Hello World DAG",
+    schedule_interval=None,  # Only run manually
+    catchup=False,
+    tags=["example"],
+)
+
+hello_task = PythonOperator(
+    task_id="hello_task",
+    python_callable=hello_world,
+    dag=dag,
+)
