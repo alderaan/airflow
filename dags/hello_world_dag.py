df7e4531a9f4d8a8fee64f7e5f4475bd70757ad2
David Lucker <david.l@gojek.com>
2025-06-23 21:04:53 +0700

    versioningg

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -1,28 +1,36 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
+#
+# This is the corrected, modern version of your DAG
+#
+from __future__ import annotations
+
+import pendulum
+
+from airflow.decorators import dag, task
 
 
+@task
 def hello_world():
+    """This is the task implementation."""
     print("Hello World, David is gonna be super successful. Really. ")
 
 
-default_args = {
-    "start_date": datetime(2024, 1, 1),
-}
-
-dag = DAG(
-    "hello_world",
-    default_args=default_args,
+@dag(
+    dag_id="hello_world",
+    start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
     description="A simple Hello World DAG",
-    schedule=None,  # Only run manually
+    schedule=None,
     catchup=False,
     tags=["example"],
-    version="1.1.0",
+    version="1.1.0",  # <-- The version parameter now works correctly here
 )
+def hello_world_dag():
+    """
+    ### Hello World DAG
+    This DAG prints a hello world message.
+    """
+    # This calls your task function to create an instance of the task
+    hello_world()
 
-hello_task = PythonOperator(
-    task_id="hello_task",
-    python_callable=hello_world,
-    dag=dag,
-)
+
+# This final line instantiates the DAG
+hello_world_dag()
